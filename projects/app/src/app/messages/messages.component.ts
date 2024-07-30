1ebacaf09a35e29135a0dda604c33b966512e094
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-07-30 18:03:29 +0300

    Update messages.component.ts

--- projects/app/src/app/messages/messages.component.ts
+++ projects/app/src/app/messages/messages.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
 import { HeroService } from '../hero.service'; // Assuming this path
-
+import { Observable, startWith, scan, switchMap } from 'rxjs';
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent {
-  messages: string[] = [];
-
+  messages$: Observable<string []>;
+  clearSubject$ = new Subject<void>();
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
-    // Subscribe to HeroService log stream (if available)
-    this.heroService.log$.subscribe(message => {
-      this.messages.push(message);
-    });
+    this.messages$ = this.clearSubject$.pipe(
+      startWith(null),
+      switchMap(() => this.heroService.log$.pipe(
+        scan((acc, message) => [...acc, message], [] as string[]),
+      ))
+    );
   }
   clearMessages() {
-    this.messages.splice(0, this.messages.length);
+    this.clearSubject$.next();
   }
 }
