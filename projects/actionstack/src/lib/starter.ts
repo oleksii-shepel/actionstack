e9bba3c5a5391adb400f4fd4987f53397dcbd971
Oleksii Shepel <113843497+oleksii-shepel@users.noreply.github.com>
2024-07-31 17:43:21 +0300

    Update starter.ts

--- projects/actionstack/src/lib/starter.ts
+++ projects/actionstack/src/lib/starter.ts
@@ -56,11 +56,11 @@ export const createStarter = () => {
      * @param {Lock} lockInstance - The lock instance to manage concurrency for this action.
      * @returns {Promise<void> | void} - A promise if the action is asynchronous, otherwise void.
      */
-    async handleAction(action: any, next: Function, lockInstance: any, source?: any) {
+    async handleAction(action: any, next: Function, lockInstance: any) {
 
       await lockInstance.acquire();
 
-      const op = Operation.action(action, source);
+      const op = Operation.action(action);
       this.stack.add(op);
 
       try {
@@ -70,7 +70,8 @@ export const createStarter = () => {
           const asyncFunc = (async () => {
             await action(
               async (syncAction: Action<any>) => {
-                  await this.handleAction(syncAction, next, innerLock, op);
+                  (syncAction as any).source = op;
+                  await this.handleAction(syncAction, next, innerLock);
               },
               this.getState,
               this.dependencies()
